be85f42027b63d3f11be6816291ef9ab0c20d75c
Raju Rumba <49477058+ts-raju@users.noreply.github.com>
2019-05-01 09:35:03 +0545

    Arithmetic Operators in Java
    Java Arithmetic Operators +, -, *, /, ^, % etc.

---
+++ ArithmeticOperator.java
@@ -0,0 +1,12 @@
+class ArithmeticOperator{
+	public static void main(String args[]){
+		int x=10; 
+		int y=2; 
+		System.out.println("Sum = "+(x+y));
+		System.out.println("Difference = "+(x-y));
+		System.out.println("Product = "+(x*y));
+		System.out.println("Quotient = "+(x/y));
+		System.out.println("Power = "+(x^y));
+		System.out.println("Modulus = "+(x%4));
+	}
+}
